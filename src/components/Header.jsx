6e55bc396bbbdcdc5884cb264cd98b599d3cbf14
Felipe Silva <felipe.silva.124@ufrn.edu.br>
2023-08-23 19:19:13 -0300

    first-app-react

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>MHelph</h1>
+            <strong>Seu blog sobre saúde mental.</strong>
+        </header>
+    )
+}
---
+++ src/components/Posts.jsx
@@ -0,0 +1,9 @@
+export default function Posts() {
+    return (
+        <><h2>Título do Post</h2><ul>
+            <li>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eius eveniet enim consectetur dignissimos repudiandae libero molestiae. Dignissimos similique ratione nisi beatae commodi ipsum reiciendis aspernatur quaerat, sint mollitia officiis.</p>
+            </li>
+        </ul></>
+    );
+}
